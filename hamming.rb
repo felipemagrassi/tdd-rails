6d387a2e067c176cc937e5fe02620923c799e38e
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-04-17 00:47:04 -0300

    Create a genetics domain

--- hamming.rb
+++ hamming.rb
@@ -2,10 +2,29 @@
 
 class Hamming
   def self.compute(strand1, strand2)
-    raise ArgumentError unless strand1.length.eql?(strand2.length)
+    @dna1 = Genetics::Strand.new(strand1)
+    @dna2 = Genetics::Strand.new(strand2)
 
-    strand1.chars.zip(strand2.chars).count do |left, right|
+    raise ArgumentError unless @dna1.length.eql?(@dna2.length)
+
+    @dna1.chars.zip(@dna2.chars).count do |left, right|
       !left.eql?(right)
+    end
+  end
+end
+
+module Genetics
+  class Strand
+    def initialize(strand)
+      @strand = strand
+    end
+
+    def length
+      @strand.length
+    end
+
+    def chars
+      @strand.chars
     end
   end
 end
