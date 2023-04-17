d4be0aa17ee67c70a2d9c1991e43dbfceda3fb67
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-04-17 00:43:56 -0300

    Use zip to compare chars different

--- hamming.rb
+++ hamming.rb
@@ -3,6 +3,9 @@
 class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError unless strand1.length.eql?(strand2.length)
-    strand1 == strand2 ? 0 : 1
+
+    strand1.chars.zip(strand2.chars).count do |left, right|
+      !left.eql?(right)
+    end
   end
 end
