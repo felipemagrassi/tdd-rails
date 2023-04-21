f2466de488d2db81da390f403f9bef7d1fcb38f2
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-04-21 19:11:47 -0300

    add rails app to test

---
+++ bio_web_app/test/integration/hamming_distance_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+class HammingDistanceTest < ActionDispatch::IntegrationTest
+  test 'happy path' do
+    visit '/'
+    click_link('Calculate Hamming Distance')
+
+    fill_in('First DNA strand', with: 'ACG')
+    fill_in('Second DNA strand', with: 'ACT')
+
+    click_button('Calculate')
+
+    assert page.has_text?('Result: 1')
+  end
+end
