a1e94a1528efb3073034f9ec840b7dcc70a005b2
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-04-22 11:39:13 -0300

    result page working

--- bio_web_app/app/controllers/hamming_distance_calculation_controller.rb
+++ bio_web_app/app/controllers/hamming_distance_calculation_controller.rb
@@ -2,4 +2,13 @@ class HammingDistanceCalculationController < ApplicationController
   def new
     @calculation = HammingDistanceCalculation.new
   end
+
+  def calculate
+    @message = "Result: #{hamming_result(params)}" end
+
+  private
+
+  def hamming_result(params)
+    '1'
+  end
 end
--- bio_web_app/app/models/hamming_distance_calculation.rb
+++ bio_web_app/app/models/hamming_distance_calculation.rb
@@ -1,3 +1,5 @@
 class HammingDistanceCalculation 
   include ActiveModel::Model
+
+  attr_accessor :dna_strand_1, :dna_strand_2
 end
